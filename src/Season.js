f6a48b0ef2c54c7c3ff07ac007c7f9498dbd29df
Pinky Gautam <57994731+ppkgtmm@users.noreply.github.com>
2020-05-29 16:09:46 +0700

    responsive seasons

--- src/Season.js
+++ src/Season.js
@@ -3,20 +3,40 @@ import './App.css';
 import Content from './Content'
 import SideBar from "./SideBar"
 import data from './data'
-function Season(props){
-
+class Season extends React.Component{
+        constructor(props) {
+            super(props);
+            this.state = {
+                sideActive : false
+            }
+            this.open = this.open.bind(this);
+        }
+        open = () => {
+            this.setState({ 
+                sideActive : true
+            });
+            // console.log(this.state.sideActive);
+        }
+        close = () => {
+            this.setState({ 
+                sideActive : false
+            });
+        }
+        render(){
         return(
                 <div className="Season">
-                    <SideBar />
                     <div className="up-nav">
-                        <div className="items">
+                        <div className="items right">
                             <a className="rand-link" href="/">Randomize</a>
                         </div>
                     </div>
+                    {!this.state.sideActive?  <button className="side-bar open" onClick={this.open}> <img src={require("./img/open-menu.png")}/></button> 
+                    : <button className="side-bar close" onClick= {this.close}>X</button>}
+                    {this.state.sideActive ? <SideBar/> : ''}
                     <div className="container">
                     <div className="Cards">
                         {
-                            data[props.season].map((info,i)=>{
+                            data[this.props.season].map((info,i)=>{
                                 return (
                                     <div className="card" key={i}>
                                         <Content name={info.name} content={info.content} url={info.imgPath} />
@@ -29,5 +49,6 @@ function Season(props){
 
 
         );
+}
 }
 export default Season
--- src/SideBar.js
+++ src/SideBar.js
@@ -1,15 +1,30 @@
 import React from "react"
 import Buttons from "./Buttons"
-function SideBar(){
+class SideBar extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            isActive : this.props.active
+        }
+        this.close = this.close.bind(this);
+    }
+    close = () => {
 
-        return(
-            <div className="sidebar pure-u-1 pure-u-md-1-4" id="yui_3_18_1_1_1582299326390_8">
-            <div className="header" id="yui_3_18_1_1_1582299326390_11">
-                <h1 className="brand-title">Travel Now</h1>
-                <h2 className="brand-tagline" id="yui_3_18_1_1_1582299326390_10">simple blog help planning vacation</h2>
-                <Buttons />
-            </div>
-        </div>
+        this.setState({ 
+            isActive : !this.state.isActive,
+        });
+    }
+    render () {
+        return (
+                <div className="sidebar pure-u-1 pure-u-md-1-4">
+                    <div className="header" id="yui_3_18_1_1_1582299326390_11">
+                        <h1 className="brand-title">Travel Now</h1>
+                        <h2 className="brand-tagline" id="yui_3_18_1_1_1582299326390_10">simple blog help planning vacation</h2>
+                        <Buttons />
+                    </div>
+                </div>
         )
+    }
+        
 }
 export default SideBar
