08d91a0e6a6f1c1af4efadb1c59f8424b459dfc0
ppkgtmm <pkgtm1998@gmail.com>
2021-03-24 12:55:24 +0700

    add random button on small screen

--- src/RandomContent.js
+++ src/RandomContent.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css'
 function RandomContent(props) {
         return(
-            <div className="random-content h-auto relative mt-8" >
+            <div className="random-content h-auto relative mt-4" >
                 <img className="m-auto" src={require("./img/"+ props.url).default} alt="not available"/>
                 <div className="data w-8/12 m-auto">
-                    <h3 className="text-center">{props.name}</h3>
-                    <p className="text-justify"> {props.content}</p>
+                    <h3 className="text-center pt-4">{props.name}</h3>
+                    <p className="text-justify pt-2"> {props.content}</p>
                 </div>
             </div>
         );
--- src/Randomize.js
+++ src/Randomize.js
@@ -3,6 +3,8 @@ import "./style/index.css"
 import SideBar from "./SideBar";
 import data from "./data";
 import RandomContent from "./RandomContent";
+import { Hamburger } from "./icons/Hanburger";
+import { Close } from "./icons/Close";
 class Randomize extends React.Component{
     constructor(props) {
         super(props);
@@ -41,29 +43,16 @@ class Randomize extends React.Component{
            
             <div className="randomize">
             <nav className="bg-black h-auto">
-            <div className="hidden md:flex p-2.5 items-center justify-between">
-            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu-2 w-6 h-6" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <line x1="4" y1="6" x2="20" y2="6" />
-                <line x1="4" y1="12" x2="20" y2="12" />
-                <line x1="4" y1="18" x2="20" y2="18" />
-            </svg>
+            <div className="flex p-2.5 items-center justify-between">
+                {!this.state.sideActive?   <button onClick={this.open}><Hamburger/></button> 
+                : <button onClick= {this.close}><Close/></button>}
             <div>
-                <button className="text-white border border-white rounded px-4 py-1 outline-none hover:bg-white hover:text-black" onClick={this.handleRandom}>randomize</button> 
+                <button className="hidden md:block text-white border border-white rounded px-4 py-1 outline-none hover:bg-white hover:text-black" onClick={this.handleRandom}>randomize</button> 
             </div>
-            </div>
-            <div class="md:hidden p-3" id="mobile-menu">
-            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu-2 w-6 h-6" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <line x1="4" y1="6" x2="20" y2="6" />
-                <line x1="4" y1="12" x2="20" y2="12" />
-                <line x1="4" y1="18" x2="20" y2="18" />
-            </svg>
             </div>
                 </nav>
-                {/* {!this.state.sideActive?  <button className="side-bar open" onClick={this.open}> <img src={require("./img/open-menu.png").default}/></button> 
-                    : <button className="side-bar close" onClick= {this.close}>X</button>} */}
-                {this.state.sideActive ? <SideBar /> : ''}
+                <button className="relative mt-8 block md:hidden text-black border border-black rounded px-4 py-1 outline-none hover:bg-black hover:text-white mx-auto" onClick={this.handleRandom}>randomize</button> 
+                {this.state.sideActive ? <SideBar /> : null}
                     <RandomContent name={this.state.data[this.state.index].name} content={this.state.data[this.state.index].content} url={this.state.data[this.state.index].imgPath}/>
             </div>
         );
