ad86d8f14b4f76ebb6a06bea17c0e942b88d64f5
ppkgtmm <pkgtm1998@gmail.com>
2021-04-12 22:34:47 +0700

    adjust margin

--- src/components/layouts/desktop/SideBar.tsx
+++ src/components/layouts/desktop/SideBar.tsx
@@ -4,7 +4,7 @@ import { seasons as seasonsData } from "../../../data"
 
 export default function SideBar() {
     return (
-        <div className="sidebar hidden md:block bg-black fixed overflow-y-scroll top-44px left-0px z-50 md:w-72 h-full">
+        <div className="hidden md:block bg-black fixed overflow-y-scroll top-44px left-0px z-50 md:w-72 h-full sidebar">
             <div className="side-content px-8">
                 <h1 className="text-white text-4xl">Travel Now</h1>
                 <h2 className="text-emerald-200 text-lg mt-4">
