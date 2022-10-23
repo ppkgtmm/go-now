7a6161861716ab1a94dedd241cf141c9bfa21e49
Pinky Gautam <57994731+ppkgtmm@users.noreply.github.com>
2022-10-23 19:49:44 +0700

    Update SideBar.tsx

--- src/components/layouts/desktop/SideBar.tsx
+++ src/components/layouts/desktop/SideBar.tsx
@@ -7,8 +7,8 @@ export default function SideBar() {
         <div className="hidden md:block bg-black fixed overflow-y-scroll top-44px left-0px z-50 md:w-72 h-full sidebar slide">
             <div className="side-content px-8">
                 <h1 className="text-white text-4xl">Travel Now</h1>
-                <h2 className="text-emerald-200 text-lg mt-4">
-                    simple site choosing vacation venue
+                <h2 className="text-teal-100 text-lg mt-4">
+                    catalouge to help choose vacation venue
                 </h2>
                 <Buttons buttonsData={seasonsData} />
             </div>
