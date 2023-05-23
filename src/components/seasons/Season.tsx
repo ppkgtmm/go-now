a6c086dccc9b71da40397603fe8b01a2068eab0d
Pinky Gautam <pkgtm1998@gmail.com>
2023-05-23 18:18:25 +0700

    update card

--- src/components/seasons/Season.tsx
+++ src/components/seasons/Season.tsx
@@ -8,13 +8,13 @@ type Props = {
 export default function Season(props: Props) {
     const cardStyle = {
         container:
-            "w-72 rounded-md md:rounded-lg shadow-lg overflow-hidden md:hover:shadow-xl text-sm my-4 mx-auto md:m-0 season",
+            "w-64 md:w-72 rounded-md md:rounded-lg shadow-lg overflow-hidden md:hover:shadow-xl text-sm my-4 mx-auto md:m-0 card",
         textContainer: "p-4",
     }
     return (
         <Layout isHomePage={false}>
             <div className="mt-20 mb-6">
-                <div className="container mx-auto md:max-w-screen-sm lg:max-w-screen-lg md:grid grid-cols-2 lg:grid-cols-3 items-stretch justify-items-center md:gap-x-2 md:gap-y-8 lg:gap-x-8">
+                <div className="container mx-auto md:max-w-screen-sm lg:max-w-screen-lg grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-stretch justify-items-center md:gap-x-2 md:gap-y-8 lg:gap-x-8">
                     {(props.places || []).map((place, index) => (
                         <Content data={place} key={index} style={cardStyle} />
                     ))}
