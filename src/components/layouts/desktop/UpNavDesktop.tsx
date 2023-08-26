294026731c6593f731e3bd35b940db448d41b560
Pinky Gautam <pkgtm1998@gmail.com>
2023-08-26 18:27:04 +0700

    Update UpNavDesktop.tsx

--- src/components/layouts/desktop/UpNavDesktop.tsx
+++ src/components/layouts/desktop/UpNavDesktop.tsx
@@ -9,9 +9,10 @@ export default function UpNavDesktop(props: Props) {
         "hidden md:block border border-white text-white rounded hover:bg-white hover:text-black px-8 py-1 leading-6"
     return (
         <header className="fixed top-0 z-50 hidden md:block bg-black w-full h-auto mt-0">
-            <div className="flex p-2.5 items-center justify-between min-h-[54px]">
+            <div className="flex p-2.5 items-center justify-between">
                 <Toggler />
                 <HomeButton {...props} buttonClass={buttonClass} text='home' />
+                {if(isHomePage) {<div className="h-[34px]"></div>}}
             </div>
         </header>
     )
